03aca97bd3d8de435459f4cef53d26b6a977e7a6
Javlon Alimov <javlonalimov1@gmail.com>
2023-07-08 10:27:45 +0500

    First change

--- Program.cs
+++ Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Sonni top o'yini");
+            Console.WriteLine("");
+            int randomSon = new Random().Next(1, 100);
+            int urinishlarSoni = 1;
+            Console.Write("Son kiriting :");
+            int taxmin = Convert.ToInt32(Console.ReadLine());
+            while (taxmin != randomSon)
+            {
+                if (taxmin > randomSon)
+                {
+                    Console.WriteLine("Men o'ylagan son kichikroq!");
+                    Console.Write("Qaytadan son kiriting :");
+                    taxmin = Convert.ToInt32(Console.ReadLine());
+                    urinishlarSoni++;
+                }
+                else if (taxmin < randomSon)
+                {
+                    Console.WriteLine("Men o'ylagan soni kattaroq!");
+                    Console.Write("Qaytadan son kiriting :");
+                    taxmin = Convert.ToInt32(Console.ReadLine());
+                    urinishlarSoni++;
+                }
+            }
+
+            Console.WriteLine($"Tabriklayman siz o'ylagan sonimni " +
+                $"{urinishlarSoni} ta urinishda topdingiz!");
         }
     }
 }
